10d6b2507acb12e1b0db647b35f2b023c67e3f43
anmachar <anmachar@DIN73003054.corp.capgemini.com>
2018-11-23 14:40:43 +0530

    commit

---
+++ Demo_day1/src/main/java/codedemo/Demo.java
@@ -0,0 +1,13 @@
+package codedemo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		hello();
+	}
+	public static void hello() {
+		System.out.println("chotu");
+	}
+
+}
